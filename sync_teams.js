7ada1e380a8d7bcad5f043333a342ef91f9a983e
BibleProg <45607940+BibleProg@users.noreply.github.com>
2022-10-17 23:04:03 +0200

    on CONTINUE

--- sync_teams.js
+++ sync_teams.js
@@ -6,30 +6,95 @@
 //     https://api.github.com/gists 
 //     -data "{\"description\":\"Example of a gist\",\"public\":false,\"files\":{\"README.md\":{\"content\":\"Hello World\"}}}"
 
+// create POST
+// token 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
+// var data = '{"description":"Example of a gist","public":false,"files":{"README.md":{"content":"Hello World"}}}';
 
-    console.log("Execute ShowdownSync !");
-
-    var token = "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"
-
-    var url = "https://api.github.com/gists";
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url);
-
-    xhr.setRequestHeader("Accept", "application/vnd.github+json");
-    xhr.setRequestHeader("Authorization", "Bearer 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");
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+console.log("Execute ShowdownSync !");
+token    = "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";
+username = "BibleProg";
+debug    = true;
+gistName = "ShowdownSync";
+id = "";
 
 
-    xhr.onreadystatechange = function () {
-       if (xhr.readyState === 4) {
-          console.log(xhr.status);
-          console.log(xhr.responseText);
-       }};
 
-    var data = '{"description":"Example of a gist","public":false,"files":{"README.md":{"content":"Hello World"}}}';
-    xhr.send(data);
+function Requete(url, method="GET", data=false){
 
+   var url = "https://api.github.com/gists";
+   var xhr = new XMLHttpRequest();
+   
+   xhr.open(method, url, false); // false force sync
 
-    // teams = localStorage.getItem('showdown_teams');
-    // console.log(teams);
+   xhr.setRequestHeader("Accept", "application/vnd.github+json");
+   xhr.setRequestHeader("Authorization", "Bearer " + token);
+
+   // xhr.onreadystatechange = function () {
+   //  if (xhr.readyState === 4 && debug) {
+   //     console.log(xhr.status);
+   //     console.log(xhr.responseText);
+   // }};
+
+   xhr.send(data);
+   // console.log( xhr.responseText);
+   return {
+    'status': xhr.status,
+    'text': xhr.responseText //.replace("[","").replace("]","").replace("\n","").replace(" ", "")
+  };
+
+}
+
+function Create(){
+
+   var data = '{"description":"Example of a gist","public":false,"files":{"README.md":{"content":"Hello World"}}}';
+   var url  = "https://api.github.com/gists";
+   Requete(url, "POST", data);
+}
+
+function Exist(){
+
+   var url = "https://api.github.com/users/" + username + "/gists";
+
+   var res = Requete(url); 
+   var gists = JSON.parse(res.text); 
+
+   // console.debug(gists);
+
+   if (gists.length > 0){
+      for(var i = 0; i < gists.length; i++){
+         
+            var description = gists[i].description;
+            id          = gists[i].id;
+            if (description == gistName){
+               return true;
+            }
+      }
+
+   }
+
+   return false;
+
+}
+
+function Get(){
+   var url = "https://api.github.com/gists/" + id;
+   var res = Requete(url);
+   var gist = JSON.parse(res.text);
+   console.debug(gist[0].files);
+
+   return gist[0].files.content;
+}
+
+console.debug(Exist());
+// var ok = Exist();
+if (Exist()){
+   rTeams = Get();
+   console.log(rTeams);
+
+}
+// lTeams = localStorage.getItem('showdown_teams');
+// if(lTeams === null){
+//    localStorage.getItem
+// }
+// console.debug(lTeams);
+
