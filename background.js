e8c4c21c9b75a88f644694b75279b016537fffc5
BibleProg <45607940+BibleProg@users.noreply.github.com>
2022-10-26 13:28:35 +0200

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-function monitorEvents(element) {
-  for (const key in element) {
-    if (key.startsWith('on')) {
-      console.debug(key);
-      element.addEventListener(key.slice(2), console.log);
-    }
-  }
-}
-monitorEvents(window);
